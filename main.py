ccdeed13cec85ac3071480b2b7ec54b08d4ea6aa
Dennis Agbemenu <105249635+dagbeme1@users.noreply.github.com>
2023-04-24 16:10:29 -0500

    # if credits have scrolled off the top, return to main game loop

--- main.py
+++ main.py
@@ -404,19 +404,14 @@ def game_over_screen(background_counter):
   
 def game_over_screen():
 
-    restart_button = pygame.Surface((200, 70), pygame.SRCALPHA)
-    restart_button_rect = restart_button.get_rect(center=(WIDTH // 2, HEIGHT // 2.7))
     # create list of credits to display
     credits = ["Credits:", "Game created by John Doe", "Artwork by Jane Smith", "Music by Joe Brown"]
-    restart_text = font.render("", True, WHITE)
+
     # create text surfaces for each credit line
     credit_surfaces = [font.render(credit, True, WHITE) for credit in credits]
 
     # start credits at the bottom of the screen
     credit_y = HEIGHT
-
-    background_counter = 0  # Line to initialize the counter for screen flickering
-
 
     while True:
         for event in pygame.event.get():
@@ -426,7 +421,7 @@ def game_over_screen():
                 if restart_button_rect.collidepoint(event.pos):
                     return True
 
-        background_counter = 0  # Update the counter  
+        background_counter = 0   # Line to initialize the counter for screen flickering  
 
         # Flicker the game over background using the counter
         if background_counter % 32 < 16:
@@ -436,6 +431,18 @@ def game_over_screen():
 
         screen.blit(game_over_text, game_over_rect)
         screen.blit(restart_button, restart_button_rect)
+
+        # display credits
+        for credit_surface in credit_surfaces:
+            screen.blit(credit_surface, (WIDTH/2 - credit_surface.get_width()/2, credit_y))
+            credit_y += 50  # space between each credit line
+
+        # scroll credits up
+        credit_y -= 1
+
+        # if credits have scrolled off the top, return to main game loop
+        if credit_y < -50 * len(credits):
+            return False        
 
         pygame.display.flip()
         clock.tick(FPS)

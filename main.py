553a56c6610f77b1da4de2a75f2aae2d44cb2bb2
Dennis Agbemenu <105249635+dagbeme1@users.noreply.github.com>
2023-04-24 21:15:56 -0500

    update

--- main.py
+++ main.py
@@ -425,7 +425,7 @@ def game_over_screen():
         # Render the credits onto the credits frame
         #credits_frame.fill((255, 255, 255))  # Clear the frame
 
-        credits_speed = 1
+        credits_speed += 8
         # Update the position of the text
         credit_rect.move_ip(0, credits_speed)
 
@@ -434,7 +434,7 @@ def game_over_screen():
             screen.blit(credit_text, credit_rect)
 
         # Update the position of the credits
-        credit_y -= credits_speed
+        credit_y += credits_speed
 
         # Check if all credits have finished rolling down
         if credit_y < -len(credits_list) * credit_height:
